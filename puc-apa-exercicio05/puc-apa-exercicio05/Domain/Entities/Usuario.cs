afa50f2a28e2fa51b0e14326d18fdd986ad5f147
Ulisses Alves Faustino de Oliveira <C0007200@vli-logistica.com.br>
2020-07-10 20:50:00 -0300

    Criacao usuario

---
+++ puc-apa-exercicio05/puc-apa-exercicio05/Domain/Entities/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace puc_apa_exercicio05.Domain.Entities
+{
+	public class Usuario: BaseEntity
+	{
+		public string Cpf { get; set; }
+
+        public string Nome { get; set; }
+
+    }
+}
