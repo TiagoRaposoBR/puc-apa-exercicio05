6709d4bf23c412264e56b921274258ec32368024
Tiago Raposo <tiago.o.raposo@gmail.com>
2020-07-10 20:39:02 -0300

    Base classes

---
+++ puc-apa-exercicio05/puc-apa-exercicio05/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace puc_apa_exercicio05.Domain.Entities
+{
+	public abstract class BaseEntity
+	{
+		private int Id { get; set; }
+	}
+}
---
+++ puc-apa-exercicio05/puc-apa-exercicio05/Domain/UseCases/BaseUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace puc_apa_exercicio05.Domain.UseCases
+{
+	public abstract class BaseUseCase
+	{
+
+	}
+}
