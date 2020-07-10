d4018d970a9d25bb620d3fc816c9a821308c4d6d
Thiago Vieira Reginaldo <thiago.reginaldo@supermix.com.br>
2020-07-10 20:51:01 -0300

    FIT: regras de negócio do carrinho

--- puc-apa-exercicio05/puc-apa-exercicio05/Domain/Entities/CarrinhoEntity.cs
+++ puc-apa-exercicio05/puc-apa-exercicio05/Domain/Entities/CarrinhoEntity.cs
@@ -4,7 +4,23 @@ namespace puc_apa_exercicio05.Domain.Entities
 {
     public abstract class CarrinhoEntity : BaseEntity
     {
-        public int Id_Usuario { get; set; }
+        public int IdUsuario { get; set; }
         public List<LivroEntity> Livros { get; set; }
+
+        public int CriarCarrinho(UsuarioEntity usuario, LivroEntity primeiroItem)
+        {
+            IdUsuario = usuario.Id;
+            Livros = new List<LivroEntity>() { primeiroItem };
+        }
+
+        public int AdicionarLivro(LivroEntity livro)
+        {
+            Livros.Add(livro);
+        }
+
+        public int RemoverLivro(int livroIdARemover)
+        {
+            Livros.RemoveAll(livro => livro.Id == livroIdARemover);
+        }
     }
 }
