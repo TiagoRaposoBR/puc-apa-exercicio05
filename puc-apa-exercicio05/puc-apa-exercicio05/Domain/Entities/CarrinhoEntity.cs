c7191344dca448cf5af082df1dfa36bf2c62b457
Thiago Vieira Reginaldo <thiago.reginaldo@supermix.com.br>
2020-07-10 20:45:13 -0300

    FIT: adicionado CarrinhoEntity

---
+++ puc-apa-exercicio05/puc-apa-exercicio05/Domain/Entities/CarrinhoEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace puc_apa_exercicio05.Domain.Entities
+{
+    public abstract class CarrinhoEntity : BaseEntity
+    {
+        public int Id_Usuario { get; set; }
+        public List<LivroEntity> Livros { get; set; }
+    }
+}
