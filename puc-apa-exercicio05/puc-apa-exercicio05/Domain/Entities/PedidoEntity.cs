770a748355e19180ca9754583c857610e5093316
Sidney de Sales Leão <sidney.leao@bs2.com>
2020-07-10 21:09:31 -0300

    pedido entity

---
+++ puc-apa-exercicio05/puc-apa-exercicio05/Domain/Entities/PedidoEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace puc_apa_exercicio05.Domain.Entities
+{
+    public abstract class PedidoEntity : BaseEntity
+    {
+        private int IdUsuario { get; set; }
+        public List<LivroEntity> Livros { get; set; }
+        public int Total
+        {
+            get
+            {
+                var soma = 0;
+                foreach (var livro in Livros)
+                {
+                    soma += livro.Preco;
+                }
+                return soma;
+            }
+        }
+    }
+}
