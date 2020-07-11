cec00b33d844f28d66e6c9b3f8fb7ab9c61f143d
Ulisses Alves Faustino de Oliveira <C0007200@vli-logistica.com.br>
2020-07-10 22:01:00 -0300

    adicionar livro

--- puc-apa-exercicio05/puc-apa-exercicio05/Domain/UseCases/CasoAdicionarLivroCarrinho.cs
+++ puc-apa-exercicio05/puc-apa-exercicio05/Domain/UseCases/CasoAdicionarLivroCarrinho.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using puc_apa_exercicio05.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace puc_apa_exercicio05.Domain.UseCases
 {
-	class CasoAdicionarLivroCarrinho
+	public class CasoAdicionarLivroCarrinho
 	{
-	}
+        public CasoAdicionarLivroCarrinho(CarrinhoEntity carrinho, LivroEntity livro)
+        {
+            carrinho.AdicionarLivro(livro);
+        }
+    }
 }
